f663310588acfe518fe83986f8922be345c8144a
Rehan Dalal <rehandalal@gmail.com>
2012-08-21 19:23:40 -0400

    Add stage logging

--- createdb.js
+++ createdb.js
@@ -20,15 +20,18 @@ if (settings.pg.enabled) {
   // If no connection string is provided in settings then check for an ENV
   // variable with one.
   if (!settings.pg.connectionString) {
+    console.log("No connection string found. Checking for DATABASE_URL.");
     settings.pg.connectionString = process.env.DATABASE_URL;
   }
 
   if (settings.pg.connectionString) {
+    console.log("Connecting to: " + settings.pg.connectionString);
     var pg_client = new pg.Client(settings.pg.connectionString);
     pg_client.connect();
   }
 
   if (pg_client) {
+    console.log("Creating table `channels`");
     pg_client.query("CREATE TABLE channels (id character varying(64) NOT NULL, invited_by character varying(128), invited_at timestamp without time zone DEFAULT now());");
     pg_client.query("ALTER TABLE ONLY channels ADD CONSTRAINT channels_pkey PRIMARY KEY (id);")
   }
